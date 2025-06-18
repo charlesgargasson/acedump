45824b0813e1ace1da6a1b12e3107021882f56ec
charles <charles>
2025-06-18 03:11:19 +0200

    ok

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='acedump',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
         'ldap3',
+        'colorama',
     ],
     entry_points={
         'console_scripts': [
