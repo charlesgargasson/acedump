5cc168fa59b8ffcb0089365572fb55bbe6adcea3
charles <charles>
2025-07-02 09:41:02 +0200

    Push v1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='acedump',
-    version='0.0.11',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'ldap3 @ git+https://github.com/cannatag/ldap3',
